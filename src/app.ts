da1391d1ed96573f39713044f55116bd001675d3
codemitrayt <codemitrayt@gmail.com>
2024-08-20 20:38:31 +0530

    add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import errorHandler from "./middleware/error-handler";
+
 const app = express();
 
 app.get("/", (req, res) => {
   return res.send("Hello from BLSheet backend!");
 });
+
+app.use(errorHandler);
 
 export default app;
