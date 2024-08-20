f63e6b2221a6d59bb29cba31e0e5eb7aa44e4e84
codemitrayt <codemitrayt@gmail.com>
2024-08-20 21:54:39 +0530

    add DB_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,12 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_URL } = process.env;
 
 const Config = {
   PORT,
   NODE_ENV,
+  DB_URL,
 };
 
 export default Config;
