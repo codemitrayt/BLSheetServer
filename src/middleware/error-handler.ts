c12627026240ab2ca1a313eed1b176eb473ef2f6
codemitrayt <codemitrayt@gmail.com>
2024-08-20 20:39:11 +0530

    add error handler

---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import logger from "../config/logger";
+
+const errorHandler = (
+  error: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logger.error(error.message);
+  const statusCode = res.statusCode || error.status || 500;
+  return res.status(statusCode).json({
+    errors: [
+      {
+        type: error.name,
+        msg: error.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
+};
+
+export default errorHandler;
