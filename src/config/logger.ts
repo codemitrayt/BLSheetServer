b1b010b43c9c6a4a78d96eb83d5ec6e916201b11
codemitrayt <codemitrayt@gmail.com>
2024-08-20 20:39:05 +0530

    add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,17 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  defaultMeta: { service: "bl-sheet-service" },
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+    }),
+  ],
+});
+
+export default logger;
