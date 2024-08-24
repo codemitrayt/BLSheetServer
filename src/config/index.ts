01791aecf1a29b4a311ea40f81ba4a98824aed46
codemitrayt <codemitrayt@gmail.com>
2024-08-24 20:01:13 +0530

    add env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,30 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV, DB_URL } = process.env;
+const {
+  PORT,
+  NODE_ENV,
+  DB_URL,
+  MAIL_HOST,
+  MAIL_PORT,
+  MAIL_USERNAME,
+  MAIL_PASSWORD,
+  MAIL_FROM,
+  CREATE_PASSWORD_FRONTEND_URL,
+  JWT_SECRET,
+} = process.env;
 
 const Config = {
   PORT,
   NODE_ENV,
   DB_URL,
+  MAIL_HOST,
+  MAIL_PORT,
+  MAIL_USERNAME,
+  MAIL_PASSWORD,
+  MAIL_FROM,
+  CREATE_PASSWORD_FRONTEND_URL,
+  JWT_SECRET,
 };
 
 export default Config;
