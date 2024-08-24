dde1b99d49331fcfff5c38f8f5886ef2995d7d30
codemitrayt <codemitrayt@gmail.com>
2024-08-24 20:14:20 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import app from "./app";
+import express from "express";
+import cors from "cors";
+
 import Config from "./config";
 import connectDB from "./config/db";
 import logger from "./config/logger";
+import errorHandler from "./middleware/error-handler";
+import authRouter from "./routes/auth-routes";
+
+const app = express();
+
+const corsOption: cors.CorsOptions = {
+  origin: true,
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOption));
 
 const startServer = async () => {
   const PORT = Config.PORT;
@@ -19,3 +33,14 @@ const startServer = async () => {
 };
 
 void startServer();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Hello from BLSheet backend!");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use(errorHandler);
+
+export default app;
