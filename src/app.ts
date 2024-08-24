671257e7122842ba0a63de66eeec6e0a15ba51d1
codemitrayt <codemitrayt@gmail.com>
2024-08-24 20:00:42 +0530

    adding authroutes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import errorHandler from "./middleware/error-handler";
+import authRouter from "./routes/auth-routes";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   return res.send("Hello from BLSheet backend!");
