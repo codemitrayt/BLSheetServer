9c817c10c484ed959a4afd67d57a79149dc11169
codemitrayt <codemitrayt@gmail.com>
2024-08-26 11:04:38 +0530

    add blSheet Controller

---
+++ src/controllers/bl-sheet-controller.ts
@@ -0,0 +1,49 @@
+import { ObjectId } from "mongoose";
+import { Response, NextFunction } from "express";
+import createHttpError from "http-errors";
+import { validationResult } from "express-validator";
+
+import BLSheetService from "../services/bl-sheet-service";
+import { BLSheet, CustomRequest } from "../types";
+import AuthService from "../services/auth-service";
+
+class BLSheetController {
+  constructor(
+    private blSheetService: BLSheetService,
+    private authService: AuthService
+  ) {}
+
+  async createBLSheet(
+    req: CustomRequest<BLSheet>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const blSheet = req.body;
+    const userId = req.userId as string;
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const newBlSheet = await this.blSheetService.createBLSheet({
+      ...blSheet,
+      userId: userId as unknown as ObjectId,
+    });
+    return res.json({ message: { blSheet: newBlSheet } });
+  }
+
+  async getBLSheets(req: CustomRequest, res: Response, next: NextFunction) {
+    const userId = req.userId as string;
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const blSheets = await this.blSheetService.findBLSheetsByUserId(userId);
+    return res.json({ message: { blSheets } });
+  }
+}
+
+export default BLSheetController;
