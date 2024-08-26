c4d671488b4c0f1aa18d666cff47eb4e62409660
codemitrayt <codemitrayt@gmail.com>
2024-08-26 11:04:51 +0530

    add authenticate jwt token

---
+++ src/middleware/autenticate-jwt.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response } from "express";
+import createHttpError from "http-errors";
+
+import TokenService from "../services/token-service";
+import { CustomRequest } from "../types";
+
+const jwtToken = new TokenService();
+
+const authenticateJWT = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return next(createHttpError(401, "Unauthorized user"));
+  try {
+    const token = authHeader.split(" ")[1];
+    const user = jwtToken.verifyToken(token);
+    req.userId = user.userId;
+    next();
+  } catch (error) {
+    return next(createHttpError(401, "Unauthorized user"));
+  }
+};
+
+export default authenticateJWT;
