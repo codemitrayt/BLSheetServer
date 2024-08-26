3ad73008714c824f50f1ebf6fd3cefc54e0a6796
codemitrayt <codemitrayt@gmail.com>
2024-08-26 11:05:18 +0530

    add bl sheet routes

---
+++ src/routes/bl-sheet-routes.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response } from "express";
+
+import asyncFnHandler from "../utils/async-fn-handler";
+import BLSheetController from "../controllers/bl-sheet-controller";
+import BLSheetService from "../services/bl-sheet-service";
+import BLSheetModel from "../model/bl-sheet-model";
+import validators from "../validator";
+import authenticateJWT from "../middleware/autenticate-jwt";
+import AuthService from "../services/auth-service";
+import UserModel from "../model/user-model";
+
+const blSheetRoute = express.Router();
+const blSheetService = new BLSheetService(BLSheetModel);
+const authService = new AuthService(UserModel);
+const blSheetController = new BLSheetController(blSheetService, authService);
+
+blSheetRoute.post(
+  "/createBLSheet",
+  validators.createBlSheetBodyValidator,
+  authenticateJWT,
+  asyncFnHandler((req: Request, res: Response, next: NextFunction) =>
+    blSheetController.createBLSheet(req, res, next)
+  )
+);
+
+blSheetRoute.get(
+  "/getBLSheets",
+  authenticateJWT,
+  asyncFnHandler((req: Request, res: Response, next: NextFunction) =>
+    blSheetController.getBLSheets(req, res, next)
+  )
+);
+
+export default blSheetRoute;
