91f08ebc65175142bb74d7d47dab0540e86d4f2f
codemitrayt <codemitrayt@gmail.com>
2024-08-26 11:06:20 +0530

    add createBlSheetBodyValidator

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,11 +1,13 @@
 import sendVerificationEmailForRegistrationBodyValidator from "./validate-send-verification-email-for-registration-body";
 import createPasswordBodyValidator from "./validate-create-password-body";
 import loginUserBodyValidator from "./validate-login-user-body";
+import createBlSheetBodyValidator from "./validate-create-bl-sheet-body";
 
 const validators = {
   sendVerificationEmailForRegistrationBodyValidator,
   createPasswordBodyValidator,
   loginUserBodyValidator,
+  createBlSheetBodyValidator,
 };
 
 export default validators;
