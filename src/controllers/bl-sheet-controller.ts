add8c5f15a170207a3df4f871036f22ead6acffb
codemitrayt <codemitrayt@gmail.com>
2024-08-27 10:35:13 +0530

    add editBLSheet method

--- src/controllers/bl-sheet-controller.ts
+++ src/controllers/bl-sheet-controller.ts
@@ -63,6 +63,37 @@ class BLSheetController {
     await this.blSheetService.deleteBLSheet(blSheetId, userId);
     return res.json({ message: { deletedBlSheetId: blSheetId } });
   }
+
+  async editBLSheet(
+    req: CustomRequest<BLSheet>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const blSheet = req.body;
+    const userId = req.userId as string;
+    const blSheetId = req.query.objectId as string;
+
+    const existingBlSheet = await this.blSheetService.fingBySheetIdAndUserId(
+      blSheetId,
+      userId
+    );
+    if (!existingBlSheet)
+      return next(createHttpError(400, "Bl sheet not found"));
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const updatedBlSheet = await this.blSheetService.updateBLSheet(
+      blSheetId,
+      blSheet
+    );
+
+    return res.json({ message: { blSheet: updatedBlSheet } });
+  }
 }
 
 export default BLSheetController;
