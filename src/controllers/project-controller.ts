0e1da9f61a007cab27eae79519c869456d4b4759
codemitrayt <codemitrayt@gmail.com>
2024-09-10 15:40:44 +0530

    adding project create method

--- src/controllers/project-controller.ts
+++ src/controllers/project-controller.ts
@@ -1,6 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import createHttpError from "http-errors";
+
+import { AuthService, ProjectService } from "../services";
+import { CustomRequest, Project } from "../types";
+import { ObjectId } from "mongoose";
 
 class ProjectController {
+  constructor(
+    private projectService: ProjectService,
+    private authService: AuthService
+  ) {}
+
   getProject(req: Request, res: Response, next: NextFunction) {
     return res.json({ message: "Get Project" });
   }
@@ -9,8 +20,31 @@ class ProjectController {
     return res.json({ message: "Get Project List" });
   }
 
-  createProject(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "Create Project" });
+  async createProject(
+    req: CustomRequest<Project>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const project = req.body;
+    const userId = req.userId as string;
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const newProject = await this.projectService.createProject({
+      ...project,
+      userId: userId as unknown as ObjectId,
+    });
+
+    return res.json({
+      message: {
+        project: newProject,
+      },
+    });
   }
 
   updateProject(req: Request, res: Response, next: NextFunction) {
