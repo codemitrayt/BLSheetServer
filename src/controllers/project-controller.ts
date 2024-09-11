867111341ffc0293332756309325d8b62082b0e7
codemitrayt <codemitrayt@gmail.com>
2024-09-11 07:58:32 +0530

    add update, delete, get methods

--- src/controllers/project-controller.ts
+++ src/controllers/project-controller.ts
@@ -14,14 +14,15 @@ class ProjectController {
 
   async getProject(req: CustomRequest, res: Response, next: NextFunction) {
     const userId = req.userId as string;
+    const projectId = req.params.objectId as string;
 
     const user = await this.authService.findByUserId(userId);
     if (!user) return next(createHttpError(401, "Unauthorized"));
 
-    const result = await this.projectService.getProjectList(userId);
+    const result = await this.projectService.getProject(projectId, userId);
     return res.json({
       message: {
-        projects: result,
+        project: result,
       },
     });
   }
@@ -67,12 +68,55 @@ class ProjectController {
     });
   }
 
-  updateProject(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "Update Project" });
+  async updateProject(
+    req: CustomRequest<Project>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const project = req.body;
+    const userId = req.userId as string;
+    const projectId = req.query.objectId as string;
+
+    const existingProject = await this.projectService.findBySheetIdAndUserId(
+      projectId
+    );
+    if (!existingProject)
+      return next(createHttpError(400, "Project not found"));
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const updatedProject = await this.projectService.updateProject(
+      project,
+      projectId
+    );
+
+    return res.json({ message: { project: updatedProject } });
   }
 
-  deleteProject(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "Delete Project" });
+  async deleteProject(
+    req: CustomRequest<{ objectId: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const { objectId: projectId } = req.body;
+    const userId = req.userId as string;
+
+    console.log(projectId, userId);
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    await this.projectService.deleteProject(projectId, userId);
+    return res.json({ message: { deletedProject: projectId } });
   }
 }
 
