0a656b565b1b802b699b63df606866e2db750da9
codemitrayt <codemitrayt@gmail.com>
2024-09-14 14:11:26 +0530

    add project services

--- src/services/project-service.ts
+++ src/services/project-service.ts
@@ -1,48 +1,27 @@
-import mongoose, { PipelineStage } from "mongoose";
-import { ProjectModel } from "../model";
+import mongoose, { ObjectId, PipelineStage } from "mongoose";
+import { ProjectModel, UserModel } from "../model";
 import { Project } from "../types";
 
 class ProjectService {
-  constructor(private projectModel: typeof ProjectModel) {}
+  constructor(
+    private projectModel: typeof ProjectModel,
+    private userModel: typeof UserModel
+  ) {}
 
   async getProject(projectId: string, userId: string) {
     return this.projectModel.findOne({ _id: projectId, userId });
+  }
+
+  async getProjectById(projectId: string) {
+    return this.projectModel.findById(projectId);
   }
 
   async getProjectList(userId: string) {
     return this.projectModel.find({ userId });
   }
 
-  async getProjectListFromUserProjectArray(userId: string) {
-    const pipeline: PipelineStage[] = [
-      { $match: { userId: new mongoose.Types.ObjectId(userId) } },
-      {
-        $lookup: {
-          from: "Project",
-          localField: "projects",
-          foreignField: "_id",
-          as: "projects",
-        },
-      },
-      {
-        $addFields: {
-          isAdmin: { $eq: ["$userId", userId] },
-        },
-      },
-      {
-        $project: {
-          _id: 1,
-          name: 1,
-          description: 1,
-          img: 1,
-          tags: 1,
-          isAdmin: 1,
-        },
-      },
-    ];
-    const results = await this.projectModel.aggregate(pipeline).exec();
-    if (results.length > 0) return results;
-    return [];
+  async getProjectListFromUserProjectArray(projects: ObjectId[]) {
+    return await this.projectModel.find({ _id: { $in: projects } });
   }
 
   async createProject(project: Project) {
