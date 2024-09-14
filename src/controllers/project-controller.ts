e0ba4d57b0c81bec296df541436d01e6b1a71f7b
codemitrayt <codemitrayt@gmail.com>
2024-09-14 14:11:07 +0530

    add project invitation method

--- src/controllers/project-controller.ts
+++ src/controllers/project-controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
 import { ObjectId } from "mongoose";
@@ -18,8 +18,10 @@ import logger from "../config/logger";
 import {
   CustomRequest,
   InviteTeamMemberType,
+  ObjectIdBody,
   Project,
   ProjectMemberStatus,
+  UpdateTeamMember,
 } from "../types";
 
 class ProjectController {
@@ -44,7 +46,7 @@ class ProjectController {
     const user = await this.authService.findByUserId(userId);
     if (!user) return next(createHttpError(401, "Unauthorized"));
 
-    const project = await this.projectService.getProject(projectId, userId);
+    const project = await this.projectService.getProjectById(projectId);
     return res.json({
       message: {
         project,
@@ -67,7 +69,7 @@ class ProjectController {
     }
 
     const result = await this.projectService.getProjectListFromUserProjectArray(
-      user._id as string
+      user.projects
     );
 
     return res.json({
@@ -98,6 +100,12 @@ class ProjectController {
     });
 
     await this.authService.addProject(userId, newProject._id as string);
+    await this.projectMemberService.addProjectMember({
+      status: ProjectMemberStatus.ACCEPTED,
+      userId: userId as unknown as ObjectId,
+      projectId: newProject._id as unknown as ObjectId,
+      memberEmailId: user.email,
+    });
 
     return res.json({
       message: {
@@ -177,21 +185,21 @@ class ProjectController {
     const project = await this.projectService.getProject(projectId, userId);
     if (!project) return next(createHttpError(400, "Project not found"));
 
-    const hasInvited = await this.projectMemberService.getProjectMember(
+    let hasInvited = await this.projectMemberService.getProjectMember(
       projectId,
       email
     );
 
     if (hasInvited && hasInvited.status === ProjectMemberStatus.ACCEPTED) {
       return res.json({
-        message: `${email} this user accepted the project invitation already`,
+        message: `${email} this user accepted the project invitation already.`,
       });
     }
 
     if (!hasInvited) {
       const invitedUser = await this.authService.findUserByEmail(email);
 
-      await this.projectMemberService.addProjectMember({
+      hasInvited = await this.projectMemberService.addProjectMember({
         memberEmailId: email,
         projectId: projectId as unknown as ObjectId,
         status: ProjectMemberStatus.PENDING,
@@ -200,13 +208,15 @@ class ProjectController {
     }
 
     const inviteToken = await this.tokenService.signToken(
-      { email, projectId },
+      { email, projectId, memberId: hasInvited._id },
       "7 days"
     );
 
     const invitationLink = `${Config.FRONTEND_URL!}${
       URLS.inviteUrl
-    }?invitationToken=${inviteToken}&projectName=${project.name}`;
+    }?invitationToken=${inviteToken}&projectName=${
+      project.name
+    }&email=${email}`;
 
     await this.notificationService.send({
       to: email,
@@ -221,6 +231,102 @@ class ProjectController {
     });
 
     return res.json({ message: "Successfully sent invitation to user email." });
+  }
+
+  async getProjectMembers(
+    req: CustomRequest<ObjectIdBody>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const projectId = req.body.objectId;
+    const userId = req.userId as string;
+
+    logger.info({ projectId });
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const isProjectMember = await this.projectMemberService.getProjectMember(
+      projectId,
+      user.email
+    );
+    if (isProjectMember?.status !== ProjectMemberStatus.ACCEPTED) {
+      return next(createHttpError(400, "Unauthorized to view project members"));
+    }
+
+    const project = await this.projectService.getProjectById(projectId);
+    if (!project) return next(createHttpError(400, "Project not found"));
+
+    const projectMembers = await this.projectMemberService.getProjectMembers(
+      projectId
+    );
+
+    return res.json({
+      message: {
+        projectMembers,
+      },
+    });
+  }
+
+  async updateProjectMember(
+    req: CustomRequest<UpdateTeamMember>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const result = validationResult(req);
+    if (!result.isEmpty())
+      return next(createHttpError(400, result.array()[0].msg as string));
+
+    const { status, memberEmailId, invitationToken } = req.body;
+    const userId = req.userId as string;
+
+    const user = await this.authService.findByUserId(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const token = await this.tokenService.verifyToken(invitationToken);
+    if (!token) {
+      return next(createHttpError(400, "Invalid invitation token"));
+    }
+
+    if ((token?.exp || 0) * 1000 <= Date.now()) {
+      return next(createHttpError(400, "Invitation token expired"));
+    }
+
+    const projectMember = await this.projectMemberService.getProjectMemberById(
+      token.memberId
+    );
+
+    if (!projectMember)
+      return next(createHttpError(400, "Project member not found"));
+
+    if (projectMember.status === ProjectMemberStatus.ACCEPTED) {
+      return next(
+        createHttpError(
+          400,
+          "This user has already accepted the project invitation"
+        )
+      );
+    }
+
+    await this.projectMemberService.updateProjectMember(token.memberId, {
+      status,
+      memberEmailId,
+      projectId: projectMember.projectId as unknown as ObjectId,
+      userId: userId as unknown as ObjectId,
+    });
+
+    if (status === ProjectMemberStatus.ACCEPTED) {
+      await this.authService.addProject(
+        userId,
+        projectMember.projectId as unknown as string
+      );
+    }
+
+    return res.json({ message: `Project invitation ${status}` });
   }
 }
 
