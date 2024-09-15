1660c74cec34ca908bafbd9a88100d7c29e16833
codemitrayt <codemitrayt@gmail.com>
2024-09-15 17:07:48 +0530

    add project task router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,12 @@ import authRoutes from "./auth-routes";
 import blSheetRoutes from "./bl-sheet-routes";
 import todoRoutes from "./todo-routes";
 import projectRouters from "./project-routes";
+import projectTaskRouters from "./project-task-routes";
 
-export { authRoutes, blSheetRoutes, todoRoutes, projectRouters };
+export {
+  authRoutes,
+  blSheetRoutes,
+  todoRoutes,
+  projectRouters,
+  projectTaskRouters,
+};
