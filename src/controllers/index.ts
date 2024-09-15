ae5ed793be19386025d7833838c34639249fb1ae
codemitrayt <codemitrayt@gmail.com>
2024-09-15 17:07:11 +0530

    add project task controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,5 +2,12 @@ import AuthController from "./auth-controller";
 import BLSheetController from "./bl-sheet-controller";
 import TodoController from "./todo-controller";
 import ProjectController from "./project-controller";
+import ProjectTaskController from "./project-task-controller";
 
-export { AuthController, BLSheetController, TodoController, ProjectController };
+export {
+  AuthController,
+  BLSheetController,
+  TodoController,
+  ProjectController,
+  ProjectTaskController,
+};
