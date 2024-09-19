b99680f519d1f7b1727334ff9020dde5c91bc355
codemitrayt <codemitrayt@gmail.com>
2024-09-19 20:18:51 +0530

    Add filter for getting project members

--- src/services/project-member-service.ts
+++ src/services/project-member-service.ts
@@ -1,6 +1,6 @@
 import mongoose, { PipelineStage } from "mongoose";
 import { ProjectMemberModel } from "../model";
-import { ProjectMember } from "../types";
+import { GetProjectMemberQuery, ProjectMember } from "../types";
 
 class ProjectMemberService {
   constructor(private projectMemberModel: typeof ProjectMemberModel) {}
@@ -9,9 +9,22 @@ class ProjectMemberService {
     return await this.projectMemberModel.findById(memberId);
   }
 
-  async getProjectMembers(projectId: string, userId: string) {
+  async getProjectMembers(
+    projectId: string,
+    userId: string,
+    query: GetProjectMemberQuery
+  ) {
+    const { currentPage, perPage, memberEmail, status } = query;
+    const searchQuery = new RegExp(memberEmail, "i");
+
     const pipeline: PipelineStage[] = [
-      { $match: { projectId: new mongoose.Types.ObjectId(projectId) } },
+      {
+        $match: {
+          projectId: new mongoose.Types.ObjectId(projectId),
+          memberEmailId: { $regex: searchQuery },
+          ...(status && { status }),
+        },
+      },
       {
         $addFields: {
           isAdmin: { $eq: ["$userId", new mongoose.Types.ObjectId(userId)] },
@@ -25,10 +38,29 @@ class ProjectMemberService {
           isAdmin: 1,
         },
       },
+      {
+        $facet: {
+          metadata: [{ $count: "totalCount" }],
+          projectMembers: [
+            { $skip: (currentPage - 1) * perPage },
+            { $limit: perPage },
+          ],
+        },
+      },
+      {
+        $unwind: "$metadata",
+      },
+      {
+        $addFields: {
+          totalCount: "$metadata.totalCount",
+        },
+      },
     ];
 
     const result = await this.projectMemberModel.aggregate(pipeline).exec();
-    return result;
+    console.log(result[0]);
+    if (result.length) return result[0];
+    return { metadata: { totalCount: 0 }, projectMembers: [] };
   }
 
   async addProjectMember(projectMember: ProjectMember) {
