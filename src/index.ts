85b15ba2bfb7f144f26250831db8f89de38118d9
codemitrayt <codemitrayt@gmail.com>
2024-09-30 18:34:33 +0530

    Add route

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,11 @@ app.get("/", (req, res) => {
   return res.send("Hello from BLSheet backend!");
 });
 
+app.post("/api/v1/health", (req, res) => {
+  logger.info({ msg: "Data", data: req.body });
+  return res.json({ message: "Health OK" });
+});
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/blSheet", blSheetRoutes);
 app.use("/api/v1/todo", todoRoutes);
