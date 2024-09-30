3323e5c8cd7f81dd994d9cfe012d83fe0001fc4d
codemitrayt <codemitrayt@gmail.com>
2024-09-30 20:16:19 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ import {
 const app = express();
 const server = createServer(app);
 
-// SOCKET CONNECTION
 export const io = new Server(server, {
   cors: {
     origin: [Config.FRONTEND_URL!],
@@ -55,7 +54,6 @@ const startServer = async () => {
   try {
     await connectDB();
     logger.info("Database connected successfully.");
-
     server.listen(PORT, () => logger.info(`Server listening on ${PORT}`));
   } catch (error) {
     if (error instanceof Error) {
@@ -65,20 +63,21 @@ const startServer = async () => {
   }
 };
 
-void startServer();
-
 app.use(express.json());
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  return res.send("Hello from BLSheet backend!");
-});
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/blSheet", blSheetRoutes);
 app.use("/api/v1/todo", todoRoutes);
 app.use("/api/v1/project", projectRouters);
 app.use("/api/v1/projectTask", projectTaskRouters);
+
+app.get("/", (req, res) => {
+  return res.send("Hello from BLSheet backend!");
+});
+
 app.use(errorHandler);
+
+void startServer();
 
 export default app;
