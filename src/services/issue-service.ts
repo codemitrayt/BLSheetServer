4214347e087f9428ee48129039187b0cfdabd36a
codemitrayt <codemitrayt@gmail.com>
2024-10-11 09:12:50 +0530

    Add issue service

--- src/services/issue-service.ts
+++ src/services/issue-service.ts
@@ -1,10 +1,58 @@
+import mongoose, { PipelineStage } from "mongoose";
 import { IssueModel } from "../model";
 
 class IssueService {
   constructor(private issueModel: typeof IssueModel) {}
 
-  async findIssueById(issueId: string) {
-    return await this.issueModel.findById(issueId);
+  async findIssueById(issueId: string, userId: string) {
+    const pipeline: PipelineStage[] = [
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(issueId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "author",
+        },
+      },
+      { $unwind: "$author" },
+      {
+        $addFields: {
+          isAuthor: {
+            $eq: ["$author._id", new mongoose.Types.ObjectId(userId)],
+          },
+          commentCount: { $size: "$comments" },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          updatedAt: 1,
+          labels: 1,
+          status: 1,
+          priority: 1,
+          projectId: 1,
+          closedIssueDate: 1,
+          author: {
+            _id: 1,
+            fullName: 1,
+            email: 1,
+          },
+          isAuthor: 1,
+          commentCount: 1,
+          description: 1,
+          createdAt: 1,
+        },
+      },
+    ];
+    const result = await this.issueModel.aggregate(pipeline).exec();
+    if (result.length) return result[0];
+    return null;
   }
 
   async findIssuesByProjectId(projectId: string) {
