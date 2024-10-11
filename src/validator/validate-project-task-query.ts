55841daccac1a4f2f49a89ff9e17b853bb79d36a
codemitrayt <codemitrayt@gmail.com>
2024-10-11 12:23:26 +0530

    Add query parameter

--- src/validator/validate-project-task-query.ts
+++ src/validator/validate-project-task-query.ts
@@ -57,6 +57,15 @@ const getProjectTaskQueryValidator = checkSchema(
       },
     },
 
+    onlyCompleted: {
+      customSanitizer: {
+        options: (value) => {
+          if (!value || value === "false") return false;
+          return true;
+        },
+      },
+    },
+
     perPage: {
       customSanitizer: {
         options: (value) => {
