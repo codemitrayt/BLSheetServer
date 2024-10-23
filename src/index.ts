b25b22d5731ca3560742e280678b16d06efba5f1
codemitrayt <codemitrayt@gmail.com>
2024-10-23 08:58:36 +0530

    add: origin

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
 });
 
 const corsOption: cors.CorsOptions = {
-  origin: [Config.FRONTEND_URL!],
+  origin: [Config.FRONTEND_URL!, Config.WWW_FRONTEND_URL!],
   // origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
